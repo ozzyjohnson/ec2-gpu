fd9ec365e9ad61b85d4b5df0cc616d456923e212
Ozzy Johnson <ec2-user@ip-10-203-140-196.ec2.internal>
2014-11-01 23:31:50 +0000

    download and build the nvidia driver

---
+++ drivers.sh
@@ -0,0 +1,26 @@
+# Versions and strings.
+ENV CUDA_DRIVER 340.46
+ENV CUDA_INSTALL http://us.download.nvidia.com/XFree86/Linux-x86_64/${CUDA_DRIVER}/NVIDIA-Linux-x86_64-${CUDA_DRIVER}.run
+
+# Install prereqs.
+yum groupinstall \
+      -y \
+      "Development tools"
+
+yum install \
+      -y \
+    docker \
+    git \
+    wget
+
+# Install CUDA drivers.
+wget \
+      $CUDA_INSTALL \
+        -P /tmp \
+        --no-verbose \
+      && chmod +x /tmp/NVIDIA-Linux-x86_64-${CUDA_DRIVER}.run \
+      && /tmp/NVIDIA-Linux-x86_64-${CUDA_DRIVER}.run \
+        -s \
+        -N \
+        --no-kernel-module \
+      && rm -rf /tmp/*
